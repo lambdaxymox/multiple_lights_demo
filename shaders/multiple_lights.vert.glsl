fd9b3e60548d612cb0708083e8a0b74dbb4ef93f
LambdaXymox <lambda.xymox@gmail.com>
2020-10-07 20:57:54 -0500

    Add shaders for multiple light types.

--- shaders/multiple_lights.vert.glsl
+++ shaders/multiple_lights.vert.glsl
@@ -1,25 +1,26 @@
 #version 330 core
 struct Camera {
-    mat4 projection;
     mat4 view;
+    mat4 projection;
 };
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
+out vec3 FragPos;
+out vec3 Normal;
+out vec3 TexCoords;
+
 uniform mat4 model;
 uniform Camera camera;
 
-out vec3 FragPos;
-out vec3 Normal;
-out vec2 TexCoords;
 
 void main() {
     FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(model))) * aNormal;  
+    Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoords = aTexCoords;
-    
+
     gl_Position = camera.projection * camera.view * vec4(FragPos, 1.0);
 }
 
